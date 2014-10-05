13ca32bd46a8ba307140429cef1ae3d952ab148c
Bryance Oyang <bobianite@gmail.com>
2014-10-04 23:03:52 -0700

    wrote vector point-wise division

--- sse_utils.h
+++ sse_utils.h
@@ -27,4 +27,8 @@ void sse_utils_vmuls(float *c, const float *a, const float *b, int len);
 void sse_utils_vmuld(double *c, const double *a, const double *b, int
 		len);
 
+void sse_utils_vdivs(float *c, const float *a, const float *b, int len);
+void sse_utils_vdivd(double *c, const double *a, const double *b, int
+		len);
+
 #endif /* !_SSE_UTILS_H_ */
